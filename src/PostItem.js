0f972a7869ca28e88636be9730feb1abb063eb1d
Eric-Guo <eric.guocz@gmail.com>
2019-07-21 10:33:04 +0800

    2.2.3 组件的state

--- src/PostItem.js
+++ src/PostItem.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
 
 class PostItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vote: 0
+    };
+  }
+
+  // 处理点赞逻辑
+  handleClick() {
+    let vote = this.state.vote;
+    vote++;
+    this.setState({
+      vote: vote
+    });
+  }
+
   render() {
     const { title, author, date } = this.props;
     return (
@@ -13,6 +29,19 @@ class PostItem extends Component {
         </div>
         <div>
           创建时间：<span>{date}</span>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              this.handleClick();
+            }}
+          >
+            点赞
+          </button>
+          &nbsp;
+          <span>
+            {this.state.vote}
+          </span>
         </div>
       </li>
     );
