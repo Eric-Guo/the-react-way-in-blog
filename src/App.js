231241b07e6a056566e01545355ee94426072aa7
Eric-Guo <eric.guocz@gmail.com>
2019-07-22 00:18:44 +0800

    3.1 Render 的新返回类型。

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return [
+      <ul>
+        <ListComponent />
+      </ul>,
+      <StringComponent />
+    ];
+  }
 }
 
-export default App;
+class ListComponent extends Component {
+  render() {
+    return [
+      <li key="A">First item</li>,
+      <li key="B">Second item</li>,
+      <li key="C">Third item</li>
+    ];
+  }
+}
+
+class StringComponent extends Component {
+  render() {
+    return "Just a strings";
+  }
+}
