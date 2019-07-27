121b376419ec69cceb9bbb60f77a95153420f4a0
Eric-Guo <eric.guocz@gmail.com>
2019-07-27 09:14:19 +0800

    3.2 错误处理

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Profile = ({ user }) => <div>name: {user.name}</div>;
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(err, info) {
+    // 显示错误UI
+    this.setState({ hasError: true });
+    // 同时输出错误日志
+    console.log(err, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oops, something went wrong!</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { name: "react" }
+    };
+  }
+
+  // 将user置为null，模拟异常
+  onClick = () => {
+    this.setState({ user: null });
+  };
+
+  render() {
+    return (
+      <div>
+        <ErrorBoundary>
+          <Profile user={this.state.user} />
+        </ErrorBoundary>
+        <button onClick={this.onClick}>更新</button>
+      </div>
+    );
+  }
 }
 
 export default App;
