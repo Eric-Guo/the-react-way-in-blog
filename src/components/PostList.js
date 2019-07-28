cee289c0eeb1e11340d41d31a3f4adc996a6b780
Eric-Guo <eric.guocz@gmail.com>
2019-07-29 06:00:57 +0800

    7.2.4 帖子列表页 发帖

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import { get } from "../utils/request";
-import url from "../utils/url";
 import PostItem from "./PostItem";
+import PostEditor from "./PostEditor";
+import { get, post } from "../utils/request";
+import url from "../utils/url";
 import "./PostList.css";
 
 class PostList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      newPost: false
     };
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleNewPost = this.handleNewPost.bind(this);
     this.refreshPostList = this.refreshPostList.bind(this);
   }
 
@@ -23,9 +28,36 @@ class PostList extends Component {
     get(url.getPostList()).then(data => {
       if (!data.error) {
         this.setState({
-          posts: data
+          posts: data,
+          newPost: false
         });
       }
+    });
+  }
+
+  // 保存帖子
+  handleSave(data) {
+    // 当前登录用户的信息和默认的点赞数，同帖子的标题和内容，共同构成最终待保存的帖子对象
+    const postData = { ...data, author: this.props.userId, vote: 0 };
+    post(url.createPost(), postData).then(data => {
+      if (!data.error) {
+        // 保存成功后，刷新帖子列表
+        this.refreshPostList();
+      }
+    });
+  }
+
+  // 取消新建帖子
+  handleCancel() {
+    this.setState({
+      newPost: false
+    });
+  }
+
+  // 新建帖子
+  handleNewPost() {
+    this.setState({
+      newPost: true
     });
   }
 
@@ -36,7 +68,7 @@ class PostList extends Component {
         <div>
           <h2>帖子列表</h2>
            {/* 只有在登录状态，才显示发帖按钮 */}
-          {userId ? <button>发帖</button> : null}
+          {userId ? <button onClick={this.handleNewPost}>发帖</button> : null}
           <ul>
             {this.state.posts.map(item =>
               <PostItem
@@ -46,6 +78,10 @@ class PostList extends Component {
             )}
           </ul>
         </div>
+        {/* 若当前正在创建新帖子，则渲染PostEditor组件 */}
+        {this.state.newPost ? (
+          <PostEditor onSave={this.handleSave} onCancel={this.handleCancel} />
+        ) : null}
       </div>
     );
   }
