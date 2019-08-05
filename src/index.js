055a18523f47d1d9e3b2e2b45c4294301a475d6f
Eric-Guo <eric.guocz@gmail.com>
2019-08-05 21:03:40 +0800

    Enable Redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
