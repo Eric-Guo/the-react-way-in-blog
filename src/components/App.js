6bdf962e6847652372d08cb3e107711d41f75893
Eric-Guo <eric.guocz@gmail.com>
2019-08-06 07:11:02 +0800

    8.3 在React中使用Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
+    <AddTodo />
+    <VisibleTodoList />
     <Footer />
   </div>
 )
