2a82ffe6d56971cf66955d7fd99fdfff97285c7a
Eric-Guo <eric.guocz@gmail.com>
2019-08-11 19:32:54 +0800

    More explicit form of reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
-import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter
-})
+function todoApp(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+  }
+}
 
 export default todoApp
