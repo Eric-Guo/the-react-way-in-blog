48c1f3575312860ac18ab4830cf6a23640d4a58c
Eric-Guo <eric.guocz@gmail.com>
2019-08-18 10:57:32 +0800

    Fix test by below blog post.
    https://www.robinwieruch.de/react-warning-cant-call-setstate-on-an-unmounted-component/

--- src/utils/AsyncComponent.js
+++ src/utils/AsyncComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 export default function asyncComponent(importComponent) {
   class AsyncComponent extends Component {
+    _isMounted = false;
+
     constructor(props) {
       super(props);
       this.state = {
@@ -10,12 +12,19 @@ export default function asyncComponent(importComponent) {
     }
 
     componentDidMount() {
+      this._isMounted = true;
       importComponent().then((mod) => {
-        this.setState({
-          // 同时兼容ES6和CommonJS的模块
-          component: mod.default ? mod.default : mod
-        });
+        if (this._isMounted) {
+          this.setState({
+            // 同时兼容ES6和CommonJS的模块
+            component: mod.default ? mod.default : mod
+          });
+        }
       });
+    }
+
+    componentWillUnmount() {
+      this._isMounted = false;
     }
 
     render() {
