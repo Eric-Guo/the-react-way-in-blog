37b589204097252015212f6f74ae0c3375a895e4
Eric-Guo <eric.guocz@gmail.com>
2019-08-18 10:41:43 +0800

    Sync with react-book official logic.

--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -1,3 +1,4 @@
+import { types as commentTypes } from "./comments";
 import { types as postTypes } from "./posts";
 
 const initialState = {};
@@ -6,8 +7,12 @@ const initialState = {};
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     // 获取评论列表和帖子列表时，更新列表数据中包含的所有作者信息
+    case commentTypes.FETCH_COMMENTS:
     case postTypes.FETCH_ALL_POSTS:
       return { ...state, ...action.users };
+    // 获取帖子详情时，只需更新当前帖子的作者信息
+    case postTypes.FETCH_POST:
+      return { ...state, [action.user.id]: action.user };
     default:
       return state;
   }
