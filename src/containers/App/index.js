09f4e0bc917eec55bfc715200926a74f21237f0b
Eric-Guo <eric.guocz@gmail.com>
2019-08-18 09:40:44 +0800

    Add loading function for app.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 import asyncComponent from "../../utils/AsyncComponent";
+import Loading from "../../components/Loading";
+import { getRequestQuantity } from "../../redux/modules/app";
 import connectRoute from "../../utils/connectRoute";
 
 const AsyncHome = connectRoute(asyncComponent(() => import("../Home")));
 const AsyncLogin = connectRoute(asyncComponent(() => import("../Login")));
 
-function App() {
+class App extends Component {
+  render() {
+  const { requestQuantity } = this.props;
   return (
-    <BrowserRouter>
+  <div>
+    <Router>
       <Switch>
         <Route exact path="/" component={AsyncHome} />
         <Route path="/login" component={AsyncLogin} />
         <Route path="/posts" component={AsyncHome} />
       </Switch>
-    </BrowserRouter>
+    </Router>
+    {requestQuantity > 0 && <Loading />}
+  </div>
   );
+  }
 }
 
-export default App;
+const mapStateToProps = (state, props) => {
+  return {
+    requestQuantity: getRequestQuantity(state)
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
