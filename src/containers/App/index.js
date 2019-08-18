64c18bbb74cb84574238922e045fdb30d5c0c1a5
Eric-Guo <eric.guocz@gmail.com>
2019-08-18 10:06:52 +0800

    Add ModalDialog to show error code.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import asyncComponent from "../../utils/AsyncComponent";
+import ModalDialog from "../../components/ModalDialog";
 import Loading from "../../components/Loading";
-import { getRequestQuantity } from "../../redux/modules/app";
+import { actions as appActions, getError, getRequestQuantity } from "../../redux/modules/app";
 import connectRoute from "../../utils/connectRoute";
 
 const AsyncHome = connectRoute(asyncComponent(() => import("../Home")));
@@ -11,7 +13,13 @@ const AsyncLogin = connectRoute(asyncComponent(() => import("../Login")));
 
 class App extends Component {
   render() {
-    const { requestQuantity } = this.props;
+    const { error, requestQuantity } = this.props;
+    const errorDialog = error && (
+      <ModalDialog onClose={this.props.removeError}>
+        {error.message || error}
+      </ModalDialog>
+    );
+
     return (
       <div>
         <Router>
@@ -21,6 +29,7 @@ class App extends Component {
             <Route path="/posts" component={AsyncHome} />
           </Switch>
         </Router>
+        {errorDialog}
         {requestQuantity > 0 && <Loading />}
       </div>
     );
@@ -29,12 +38,14 @@ class App extends Component {
 
 const mapStateToProps = (state, props) => {
   return {
+    error: getError(state),
     requestQuantity: getRequestQuantity(state)
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
+    ...bindActionCreators(appActions, dispatch)
   };
 };
 
